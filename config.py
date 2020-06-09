1ac5d322e74a3b58debfc8de1bdeff3b87a5625a
sanaalahbibi <sanaa.lahbib@gmail.com>
2020-06-09 05:53:06 +0100

    heroku settings

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'SHA256:cyD7EC8WfLADufwrJhQACtPnA+mclKSx+9lsfoDaJZ4'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
